25d5500558ba8959a8ed16b7d9080c2a6b743708
Jian Sun <sunjian@ucar.edu>
2021-11-20 01:35:45 -0700

    Add matrix multiplication examples for CPU and GPU (OpenACC and OpenMP) runs
    	new file:   matrix_multi/Makefile
    	new file:   matrix_multi/README.md
    	new file:   matrix_multi/build.sh
    	new file:   matrix_multi/matrix_mult.f90
    	new file:   matrix_multi/matrix_mult_omp.f90
    	new file:   matrix_multi/submit.sh

---
+++ matrix_multi/README.md
@@ -0,0 +1,1 @@
+This matrix multiplication is originally borrowed from https://github.com/sjsprecious/GPU_workshop/tree/CSG_tutorial/Lesson_2_matrixMult/OpenACC_FORTRAN/solution.
---
+++ matrix_multi/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Load the necessary modules (software)
+module purge
+module load ncarenv/1.3
+module load nvhpc/21.9
+module load cuda/11.4.0
+module list
+
+# Export variables for use in the Makefile
+export CUDA_ROOT_PATH="${NCAR_ROOT_CUDA}"
+export NVHPC_ROOT_PATH="${NCAR_ROOT_NVHPC}/Linux_x86_64/21.9/compilers"
+# Remove any previous build attempts
+make clean
+# Do a build
+make _OPENACC=true
