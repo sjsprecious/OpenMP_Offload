731538c2ea5ff84e8dc3e82c4e742931e99b4e2e
Jian Sun <sunjian@ucar.edu>
2022-01-28 16:03:40 -0700

    Restructure the matmul examples and add performance data/analysis

--- matrix_multi/matrix_mult.F90
+++ matrix_multi/matrix_mult.F90
@@ -6,16 +6,14 @@ program matrix_mult
    character(10) :: rowsBChar
    character(10) :: colsBChar
    integer, parameter:: DEFAULT_DIM=1024
-   integer, parameter:: LOOP_COUNT=100
+   integer, parameter:: LOOP_COUNT=10
    real, parameter:: MAT_A_VAL=3.0
    real, parameter:: MAT_B_VAL=2.0
    real, parameter:: VERIF_TOL=1.0E-6
-   integer :: i, j, k, n, rowsA, colsA, rowsB, colsB
+   integer :: i, j, k, n, ii, jj, rowsA, colsA, rowsB, colsB
    real :: t1, t2, dt
    real, allocatable, dimension(:,:) :: a, b, c_cpu, c_gpu
-   real :: tmp, secs
-   logical:: ver_flag
-
+   real :: tmp, secs, error
 
    if(COMMAND_ARGUMENT_COUNT().EQ.0) then
         rowsA = DEFAULT_DIM
@@ -69,6 +67,8 @@ program matrix_mult
 
 ! Compute matrix addition on CPU
 
+#ifdef _CPU
+
       call cpu_time(t1)
 
       do n = 1, LOOP_COUNT
@@ -87,18 +87,23 @@ program matrix_mult
       dt = t2-t1
       secs = dt/real(LOOP_COUNT)
       write(*,"('CPU Matrix Multiplication completed in ',f12.5,' secs')") secs
+
+#endif
  
-! Compute matrix addition on GPU
+! Compute matrix addition on GPU (OPENACC)
+
+#ifdef _OPENACC
 
       call cpu_time(t1)
 
       do n = 1, LOOP_COUNT
          !$acc data copyin(a,b) copyout(c_gpu)
-         !$acc parallel
-         !$acc loop collapse(2) reduction(+:tmp)
+         !$acc parallel vector_length(128)
+         !$acc loop gang vector collapse(2) reduction(+:tmp)
          do j=1,colsB
             do i=1,rowsA
                tmp = 0.0
+               !$acc loop vector reduction(+:tmp)
                do k=1,rowsB
                    tmp = tmp + a(i,k) * b(k,j)
                enddo
@@ -114,22 +119,62 @@ program matrix_mult
       secs = dt/real(LOOP_COUNT)
       write(*,"('GPU Matrix Multiplication completed in ',f12.5,' secs')") secs
 
+#endif
+
+! Compute matrix addition on GPU (OPENMP)
+
+#ifdef _OPENMP
+
+      call cpu_time(t1)
+
+      do n = 1, LOOP_COUNT
+         !$omp target data map(to:a,b) map(from:c_gpu)
+         !$omp target teams distribute parallel do simd collapse (2) reduction(+:tmp)
+         do j=1,colsB
+            do i=1,rowsA
+               tmp = 0.0
+               do k=1,rowsB
+                   tmp = tmp + a(i,k) * b(k,j)
+               enddo
+               c_gpu(i,j) = tmp
+            enddo
+         enddo
+         !$omp end target teams distribute parallel do simd
+         !$omp end target data
+      enddo
+
+      call cpu_time(t2)
+      dt = t2-t1
+      secs = dt/real(LOOP_COUNT)
+      write(*,"('GPU Matrix Multiplication completed in ',f12.5,' secs')") secs
+
+#endif
+
 ! Verify GPU results against CPU
-      ver_flag = .TRUE.
+      error = 0.d0
+      ii = 0
+      jj = 0
       jloop: do j=1,colsB
          do i=1,rowsA
-               if (abs(c_gpu(i,j)-c_cpu(i,j)) > VERIF_TOL) then
-                        write(*,"('Verification failed')")
-                        write(*,"('   1st error > tolerance encountered at C_gpu[',i6,'][',i6,']')") i, j
-                        write(*,"('   C_cpu[',i6,'][',i6,']=',f12.2,'')") i,j,c_cpu(i,j)
-                        write(*,"('   C_gpu[',i6,'][',i6,']=',f12.2,'')") i,j,c_gpu(i,j)
-                        ver_flag = .FALSE.
-                        exit jloop
-                end if
+            if (abs(c_gpu(i,j)-c_cpu(i,j)) > error) then
+                ii = i
+                jj = j
+                error = abs(c_gpu(i,j)-c_cpu(i,j))
+            end if
+         end do
+      end do jloop
+   
+      if ( error < VERIF_TOL ) then
+         write(*,"('Verification passed')")
+         write(*,"('   Max abs error = ',f25.16,' at ii = ',i6,', jj = ',i6,'')") error, ii, jj
+      else
+         write(*,"('Verification failed')")
+         write(*,"('   Max relative error > tolerance encountered at C_CPU[',i6,'][',i6,']')") ii, jj
+         write(*,"('   C_CPU[',i6,'][',i6,']=',G25.16,'')") ii,jj,c_cpu(ii,jj)
+         write(*,"('   C_GPU[',i6,'][',i6,']=',G25.16,'')") ii,jj,c_gpu(ii,jj)
+         write(*,"('   ABS(C_GPU-C_CPU) =',f25.16,'')") error
+      end if
 
-         enddo
-      enddo jloop
- 
 ! Uncomment section to print CPU and GPU results 
      
 !      write(*,"('CPU Results: ')")
@@ -147,9 +192,6 @@ program matrix_mult
 !               write(*,"(' ')")
 !      enddo
 
-      if(ver_flag) then
-         write(*,"('Verification passed')")
-      end if
 !Release Memory to cleanup program
       deallocate(a, b,c_cpu,c_gpu)
 end program matrix_mult
