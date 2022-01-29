0f65deb4574e8e982f982aff429f938fc21f8bd7
Jian Sun <sunjian@ucar.edu>
2022-01-28 19:47:50 -0700

    Update submit script for matmul

--- matrix_multi/submit.sh
+++ matrix_multi/submit.sh
@@ -1,28 +1,63 @@
 #!/bin/bash -l
-#PBS -N GPU_matrix_mult 
+#PBS -N CRAY_CPU 
 #PBS -A NTDD0004
-#PBS -l select=1:ncpus=36:mpiprocs=36:mem=300GB:ngpus=1
+#PBS -l select=1:ncpus=36:ompthreads=36:mem=300GB:ngpus=1
 #PBS -l gpu_type=v100
-#PBS -l walltime=01:59:00
+#PBS -l walltime=00:30:00
 #PBS -q casper 
 #PBS -j oe
 #PBS -k eod
 
+FCOMPILER=NVHPC
+
+if [ $FCOMPILER == "CRAY" ]; then
+
 # Run everything in the HPE container
-CRAYENV_GPU_SUPPORT=1 crayenv << EOF
+CRAYENV_GPU_SUPPORT=1 crayenv << 'EOF'
 # Load CUDA module
 module load craype-accel-nvidia70
 module unload cray-libsci_acc
 module load cudatoolkit
- 
+module list
+
+nvidia-smi
+
 # Move to the correct directory and run the executable
 echo -e "\nBeginning code output:\n-------------\n"
- 
 export OMP_NUM_THREADS=1
-
-./matrix_mult.exe 1024 1024 1024 1024
-./matrix_mult.exe 2048 2048 2048 2048
-./matrix_mult.exe 4096 4096 4096 4096
-./matrix_mult.exe 8192 8192 8192 8192
+for i in 1024 2048 4096 8192
+do
+  ./matrix_mult.exe $i $i $i $i
+done
 
 EOF
+
+elif [ $FCOMPILER == "NVHPC" ]; then
+
+# Load the necessary modules (software)
+module purge
+module load ncarenv/1.3
+module load nvhpc/21.11
+module load cuda/11.4.0
+module load ncarcompilers/0.5.0
+module list
+
+# Update LD_LIBRARY_PATH so that cuda libraries can be found
+export LD_LIBRARY_PATH=${NCAR_ROOT_CUDA}/lib64:${LD_LIBRARY_PATH}
+echo ${LD_LIBRARY_PATH}
+
+nvidia-smi
+
+# Move to the correct directory and run the executable
+echo -e "\nBeginning code output:\n-------------\n"
+export OMP_NUM_THREADS=1
+for i in 1024 2048 4096 8192
+do
+  ./matrix_mult.exe $i $i $i $i
+done
+
+else
+
+echo "Unsupport compiler: $FCOMPILER..."
+
+fi
